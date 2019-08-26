6ce1ad3c27e40df1df0ebc52213417af22f72408
silane <18604696+silane@users.noreply.github.com>
2019-08-27 00:02:04 +0900

    Add alarm job setting UI to myroom page

--- mywebapps/static/scripts/myroom.js
+++ mywebapps/static/scripts/myroom.js
@@ -2,14 +2,28 @@ window.addEventListener('DOMContentLoaded', event => {
 new Vue({
     el: '.body-content',
     data() { return {
+        jobs: [],
         alerts: [],
+        newAlarmTime: '',
     }},
+    computed: {
+        cmp() { return cmp; },
+    },
+    async created() {
+        await this.reloadJobList();
+    },
     methods: {
-        async invokeAction(actionID) {
-            actionID = encodeURIComponent(actionID);
+        async $__requestRooman(method, path, body) {
             let response;
+            body = method === 'HEAD' || method === 'GET' ?
+                undefined : JSON.stringify(body);
             try {
-                response = await fetch(`${ROOMAN_URL}action?id=${actionID}`);
+                response = await fetch(`${ROOMAN_URL}${path}`, {
+                    method: method,
+                    headers: {
+                        'Content-Type': 'application/json; charset=utf-8'},
+                    body: body,
+                });
             } catch(e) {
                 this.alerts.push({type: 'danger', message: 'ネットワークエラー'});
                 return;
@@ -29,6 +43,60 @@ new Vue({
                 this.alerts.push({type: 'danger', message: 'サーバーがエラーを返しました'});
                 return;
             }
+            return responseBody.payload;
+        },
+        async invokeAction(actionID, parameters) {
+            return await this.$__requestRooman('POST', 'action', {
+                id: actionID, parameters: parameters,
+            });
+        },
+        async newJob(jobTypeID, parameters) {
+            await this.$__requestRooman('POST', 'newjob', {
+                type_id: jobTypeID, parameters: parameters,
+            });
+        },
+        async deleteJob(jobID, parameters) {
+            await this.$__requestRooman('POST', 'deletejob', {
+                id: jobID, parameters: parameters,
+            });
+        },
+        async invokeJobAction(jobID, parameters) {
+            return await this.$__requestRooman('POST', 'jobaction', {
+                id: jobID, parameters: parameters,
+            });
+        },
+        async reloadJobList() {
+            const jobList = await this.$__requestRooman('GET', 'listjob', {
+                type_id: null,
+            });
+            this.jobs = await Promise.all(jobList.map(async x => {
+                const jobID = x.job_id, jobTypeID = x.job_type_id;
+                const ret = {jobID, jobTypeID, state: null, error: null};
+                switch(jobTypeID) {
+                    case 'alarm':
+                        const state = await this.invokeJobAction(jobID);
+                        if(state == null) {
+                            ret.error = 'error_code';
+                        } else {
+                            let time = state.time;
+                            time = new Time(Math.floor(time / 3600),
+                                            Math.floor(time / 60) % 60,
+                                            time % 60);
+                            ret.state = {time};
+                        }
+                        break;
+                }
+                return ret;
+            }));
+        },
+        async newAlarm() {
+            let time = this.newAlarmTime;
+            if(!time) return;
+            time = time.split(':');
+            if(time.length === 2) time.push('0');
+            time = parseInt(time[0]) * 3600 + parseInt(time[1]) * 60 +
+                   parseInt(time[2]);
+            await this.newJob('alarm', {time: time});
         },
     },
 });
