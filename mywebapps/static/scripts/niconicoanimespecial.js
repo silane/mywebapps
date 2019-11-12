d06769b9f03bd42968a0a6670fb0aabcae8e4a5c
silane <18604696+silane@users.noreply.github.com>
2019-11-13 02:43:14 +0900

    Change indentation, etc. of niconicoanimespecial.js

--- mywebapps/static/scripts/niconicoanimespecial.js
+++ mywebapps/static/scripts/niconicoanimespecial.js
@@ -1,34 +1,42 @@
 ﻿const vm = new Vue({
-    el: '.body-content',
-    data: { alerts: [], programs: [] },
-    methods: {
-        reload: function () {
-            const reloadicon = $(".reloadicon")
-            if (reloadicon.hasClass("active"))
-                return
-            reloadicon.addClass("active")
+  el: '.body-content',
+  data: { alerts: [], programs: [] },
+  methods: {
+    reload: function () {
+      const reloadicon = $(".reloadicon");
+      if (reloadicon.hasClass("active"))
+        return;
+      reloadicon.addClass("active");
 
-            function fn_finally() {
-                reloadicon.removeClass("active")
-            }
-                
-            fetch('get').then(response => {
-                if (response.ok)
-		    response.json().then(resp => {
-                        for(program of resp)
-                            program.startTime = new Date(program.startTime)
-                        this.programs = resp
-                    }, error => {
-                        this.alerts.unshift({type: 'danger', message: `Server returned invalid response: ${error}`})
-                    })
-                else
-                    this.alerts.unshift({type: 'danger', message: `Server returned status code ${response.status}`})
-            }, error => {
-                this.alerts.unshift({type: 'danger', message: `Could not get response from server: ${error}`})
-            }).then(fn_finally, fn_finally)
-            
-        },
+      function fn_finally() {
+        reloadicon.removeClass("active");
+      }
+          
+      fetch('get').then(response => {
+        if (response.ok)
+          response.json().then(resp => {
+            for(program of resp)
+              program.startTime = new Date(program.startTime);
+            this.programs = resp;
+          }, error => {
+            this.alerts.unshift({
+              type: 'danger',
+              message: `Server returned invalid response: ${error}`,
+            });
+          });
+        else
+          this.alerts.unshift({
+            type: 'danger',
+            message: `Server returned status code ${response.status}`,
+          });
+        }, error => {
+          this.alerts.unshift({
+            type: 'danger',
+            message: `Could not get response from server: ${error}`,
+          });
+        }).then(fn_finally, fn_finally);        
     },
+  },
 });
 
 vm.reload();
